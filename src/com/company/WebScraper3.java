19e2381f937cde1acbce53c184df337e688f5c61
Lydia <lydiakw2@illinois.edu>
2018-02-06 12:33:44 -0600

    write functions for word counter and edit WebScraper

--- src/com/company/WebScraper3.java
+++ src/com/company/WebScraper3.java
@@ -12,23 +12,7 @@ import java.util.Scanner;
 
 public class WebScraper3 {
 
-    public static String urlToString(final String url) {
-        Scanner urlScanner;
-        try {
-            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
-        } catch (IOException e) {
-            return "";
-        }
-        String contents = urlScanner.useDelimiter("\\A").next();
-        urlScanner.close();
-        return contents;
-    }
 
-
-    public static int wordCounter;{
-        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
-
-    }
 
 
 
