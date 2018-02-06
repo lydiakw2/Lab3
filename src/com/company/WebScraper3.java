791bb10290db52dd40883f2a86d8ed822766dcd7
Lydia <lydiakw2@illinois.edu>
2018-02-06 12:33:10 -0600

    write functions for word counter

--- src/com/company/WebScraper3.java
+++ src/com/company/WebScraper3.java
@@ -10,8 +10,7 @@ import java.util.Scanner;
  * @return the contents from the url as a string, or an empty string on error
  */
 
-public class Main {
-
+public class WebScraper3 {
 
     public static String urlToString(final String url) {
         Scanner urlScanner;
@@ -26,32 +25,13 @@ public class Main {
     }
 
 
-    public static void main (String[] args) {
-        System.out.println("http://erdani.com/tdpl/hamlet.txt");
-        System.out.println(wordCounter());
+    public static int wordCounter;{
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
 
     }
 
-    public static int wordCounter(){
-        String a = urlToString("http://erdani.com/tdpl/hamlet.txt");
-        if (a == null || a.isEmpty()){
-            return 0;
-        }
-
-        String[] textArray = a.split("\\s+");
-        int count = 0;
-        for (int i = 0; i < textArray.length; i++){
-            if ((textArray[i].toUpperCase()).contains("A")){
-                count++;
-            }
 
 
-        }
-        return count;
 
 
-    }
 }
-
-
-
